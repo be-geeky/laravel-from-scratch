4265e077acd873bfb04cf8e7b37dd558d0bc62d8
Dushyant Joshi <dushyantjoshia@gmail.com>
2017-10-24 18:59:33 +0530

    First commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1> Laracasts, <?= $name ?> </h1>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	$tasks = DB::table('tasks')->latest()->get();
+
+    return view('welcome', [
+    		'name' => 'world',
+    		'tasks' => $tasks
+    	]);
+});
+
+Route::get('/tasks/{task}', function ($id) {
+	
+	$tasks = DB::table('tasks')->latest()->find($id);
+
+	dd($tasks);
+    return view('welcome', [
+    		'name' => 'world',
+    		'tasks' => $tasks
+    	]);
+});
