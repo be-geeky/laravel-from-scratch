3d048bb91e2a54c35d39b7a01f15055c0d9693ee
Dushyant Joshi <dushyantjoshia@gmail.com>
2017-10-27 19:05:37 +0530

    layouts and forms continue...

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,4 +10,8 @@ class PostsController extends Controller {
 
 		return view('posts.show');
 	}
+	public function create() {
+
+		return view('posts.create');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostsController@index');
+
+Route::get('/posts/create', 'PostsController@create');
+
 Route::get('/posts/{post}', 'PostsController@show');
